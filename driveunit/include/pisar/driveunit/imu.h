a71a724c04ee04ff2ad783c0731391df50714534
Gabe Vaughan <gvaughan@uwaterloo.ca>
2025-03-18 18:08:23 -0400

    IMU calibration zpp and littleFS done (thx ash)

--- driveunit/include/pisar/driveunit/imu.h
+++ driveunit/include/pisar/driveunit/imu.h
@@ -3,9 +3,11 @@
 #include "pisar/driveunit/logging.h"
 
 #include <Eigen/Dense>
+#include <zpp_bits.h>
 
 #include <SPI.h>
 #include <LSM6DSOSensor.h>
+#include <LittleFS.h>
 
 #include <cstdint>
 #include <span>
@@ -15,20 +17,6 @@
 
 namespace pisar::driveunit
 {
-
-/**
- * @brief Stores calibration offsets for accelerometer and gyroscope.
- */
-struct ImuCalibrationData
-{
-    Eigen::Vector3<int16_t> accel_offset; ///< Accelerometer offset values
-    Eigen::Vector3<int16_t> gyro_offset;  ///< Gyroscope offset values
-
-    // constexpr static auto serialize(auto& archive, auto& self)
-    // {
-    //     return archive(self.accel_offset, self.gyro_offset);
-    // }
-};
 
 /**
  * @brief Provides an interface for a 6-axis Imu sensor using SPI.
@@ -57,18 +45,44 @@ public:
         GyroData gyro_data;
     };
 
-private:
-    SPIClassRP2040 &m_spi;  ///< SPI bus reference.
-    uint8_t m_cs_pin; ///< Chip Select (CS) pin.
-    uint8_t m_rx_pin; ///< Chip Select (CS) pin.
-    uint8_t m_tx_pin; ///< Chip Select (CS) pin.
-    uint8_t m_sck_pin; ///< Chip Select (CS) pin.
-    uint16_t m_sample_rate;                     ///< Sample rate.
-    std::chrono::microseconds m_sample_time;    ///< Sample time.
-    LSM6DSOSensor m_imu;                        ///< Underlying imu sensor driver.
-    Eigen::Vector3<int16_t> m_accel_offset{0, 0, 0}; ///< Offset for accelerometer.
-    Eigen::Vector3<int16_t> m_gyro_offset{0, 0, 0};  ///< Offset for gyroscope.
+    /**
+     * @brief Stores calibration offsets for accelerometer and gyroscope.
+     */
+    struct CalibrationData
+    {
+        using serialize = zpp::bits::members<2>;
 
+        Eigen::Vector3<DataRawValueT> accel_offset; ///< Accelerometer offset values
+        Eigen::Vector3<DataRawValueT> gyro_offset;  ///< Gyroscope offset values
+
+        /**
+         * @brief Saves IMU calibration data to LittleFS.
+         * @param file_path The location to save to.
+         * @return True if the data was successfully saved, false otherwise.
+         */
+        [[nodiscard]] bool save(std::string_view file_path) const;
+
+        /**
+         * @brief Loads IMU calibration data from LittleFS.
+         * @param file_path The location to load from.
+         * @return True if the data was successfully loaded, false otherwise.
+         */
+        [[nodiscard]] bool load(std::string_view file_path);
+    };
+
+private:
+
+    SPIClassRP2040 &m_spi;  ///< SPI bus reference.
+    uint8_t m_cs_pin;                               ///< Chip Select (CS) pin.
+    uint8_t m_rx_pin;                               ///< RX (MISO) pin.
+    uint8_t m_tx_pin;                               ///< TX (MOSI) pin.
+    uint8_t m_sck_pin;                              ///< Clock (SCK) pin.
+    std::string_view m_calibration_data_file_path;  ///< Calibration data file path.
+    uint16_t m_sample_rate;                         ///< Sample rate.
+    std::chrono::microseconds m_sample_time;        ///< Sample time.
+    LSM6DSOSensor m_imu;                            ///< Underlying imu sensor driver.
+
+    CalibrationData m_calibration_data; ///< Calibration data.
 
 public:
 
@@ -79,10 +93,16 @@ public:
      * @param rx_pin The SPI RX pin.
      * @param tx_pin The SPI TX pin.
      * @param sck_pin The SPI SCK pin.
+     * @param calibration_data_file_path The path to the calibration data file.
      * @param sample_rate The accelerometer and gyroscope sample rate (default: 800).
      * @param spi_speed The SPI communication speed in Hz (default: 1000000).
      */
-    Imu(SPIClassRP2040 &spi, uint8_t cs_pin, uint8_t rx_pin, uint8_t tx_pin, uint8_t sck_pin, uint16_t sample_rate = 800, uint32_t spi_speed = 1'000'000);
+    Imu(
+        SPIClassRP2040 &spi, 
+        uint8_t cs_pin, uint8_t rx_pin, uint8_t tx_pin, uint8_t sck_pin, 
+        std::string_view calibration_data_file_path, 
+        uint16_t sample_rate = 800, uint32_t spi_speed = 1'000'000
+    );
 
     /// @brief Destructor
     ~Imu();
@@ -135,7 +155,7 @@ public:
             return {};
         }
 
-        data.values -= m_accel_offset;
+        data.values -= m_calibration_data.accel_offset;
 
         return data;
     }
@@ -155,7 +175,7 @@ public:
 
         float sensitivity = getXSensitivity();
 
-        data.values = (data.values).cast<int32_t>() - (m_accel_offset.cast<float>() * sensitivity).cast<int32_t>();
+        data.values = (data.values).cast<int32_t>() - (m_calibration_data.accel_offset.cast<float>() * sensitivity).cast<int32_t>();
 
         return data;
     }
@@ -173,7 +193,7 @@ public:
             return {};
         }
 
-        data.values -= m_gyro_offset;
+        data.values -= m_calibration_data.gyro_offset;
 
         return data;
     }
@@ -193,7 +213,7 @@ public:
 
         float sensitivity = getGSensitivity();
 
-        data.values = (data.values).cast<int32_t>() - (m_gyro_offset.cast<float>() * sensitivity).cast<int32_t>();
+        data.values = (data.values).cast<int32_t>() - (m_calibration_data.gyro_offset.cast<float>() * sensitivity).cast<int32_t>();
 
         return data;
     }
@@ -294,18 +314,53 @@ public:
      * @brief Calibrates the IMU by computing offsets for the accelerometer and gyroscope.
      *
      * @param num_samples The number of samples to collect for calibration.
+     * @param save Whether to save the calibration data to the filesystem.
+     * @return True if calibration (and saving) was successful, false otherwise.
      *
      * @note Ensure the IMU is motionless during calibration to get accurate offsets.
      * @note Offsets are stored as integer values to match the IMU's processed data format.
      * @note Max number of samples is 65'535.
      */
-    void calibrate(size_t num_samples);
+    bool calibrate(size_t num_samples, bool save = true);
 
-    void setCalibration(const ImuCalibrationData& calib_data);
-    void getCalibration(ImuCalibrationData& calib_data) const;
+    /**
+     * @brief Sets the calibration data.
+     * @param calib_data The calibration data to set.
+     */
+    inline void setCalibration(const CalibrationData& calib_data)
+    {
+        m_calibration_data = calib_data;
+    }
 
-    [[nodiscard]] bool loadCalibrationData();
-    [[nodiscard]] bool saveCalibrationData() const;
+    /// @brief Gets the calibration data.
+    [[nodiscard]] inline CalibrationData getCalibration() const
+    {
+        return m_calibration_data;
+    }
+
+    /// @brief Get whether the calibration data has been saved.
+    [[nodiscard]] inline bool calibrationDataSaved() const
+    {
+        return LittleFS.exists(m_calibration_data_file_path.data());
+    }
+
+    /**
+     * @brief Loads the calibration data from the filesystem.
+     * @return True if the data was successfully loaded, false otherwise.
+     */
+    [[nodiscard]] inline bool loadCalibrationData()
+    {
+        return m_calibration_data.load(m_calibration_data_file_path);
+    }
+
+    /**
+     * @brief Saves the calibration data to the filesystem.
+     * @return True if the data was successfully saved, false otherwise.
+     */
+    [[nodiscard]] inline bool saveCalibrationData() const
+    {
+        return m_calibration_data.save(m_calibration_data_file_path);
+    }
 
     /**
      * @brief Reads the stream of raw accelerometer and gyroscope samples from the fifo.
