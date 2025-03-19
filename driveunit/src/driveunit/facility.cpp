54e918da44140077217664f664010c82438e7d06
Gabe Vaughan <gvaughan@uwaterloo.ca>
2025-03-19 14:58:48 -0400

    super broken mb

--- driveunit/src/driveunit/facility.cpp
+++ driveunit/src/driveunit/facility.cpp
@@ -16,11 +16,21 @@ void RobotFacility::updateKinematicTracker()
         Lock<Mutex> lock(m_imu_mutex);
         sample_time = m_imu.getSampleTime();
         time_stamp = static_cast<std::chrono::microseconds>(micros()); // Get current timestamp
-        data_samples = m_imu.readFifo(std::span(imu_data));
+        data_samples = m_imu.readFifoPaired(std::span(imu_data));
     }
 
     for (int i = 0; i < data_samples; ++i)
     {
+        PISAR_LOG_INFO("Sample %d: Accel[%f, %f, %f] | Gyro[%f, %f, %f] | Timestamp: %lld",
+            i,
+            imu_data[i].accel_data.values.x(),
+            imu_data[i].accel_data.values.y(),
+            imu_data[i].accel_data.values.z(),
+            imu_data[i].gyro_data.values.x(),
+            imu_data[i].gyro_data.values.y(),
+            imu_data[i].gyro_data.values.z(),
+            time_stamp.count() - (data_samples - 1 - i) * sample_time.count());
+
         m_kinematic_tracker.onImuReading(
             imu_data[i].accel_data,
             imu_data[i].gyro_data,
